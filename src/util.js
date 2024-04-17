3474a9b61c6eb6799d5c518f1e7234a27e3b2ba5
David Jennes <david.jennes@gmail.com>
2024-04-17 03:35:40 +0200

    Fix typo (issueRecord -> issueRecords)

--- src/util.js
+++ src/util.js
@@ -122,7 +122,7 @@ export async function validateCAARecords(host) {
         return null;
     }
 
-    const issueRecord = resolve.data.Answer.filter(x =>
+    const issueRecords = resolve.data.Answer.filter(x =>
         x.type == 257 && typeof x.data === 'string' && x.data.startsWith('0 issue ')
     ).map(x => x.data);
 
